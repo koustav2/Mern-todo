bcf68db16c6df9c531b206ebedb6c69a056d454e
Koustav <koustavm.aeie2018@nsec.ac.in>
2023-11-21 23:30:21 +0530

    ss

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,15 @@ require("dotenv").config('./.env');
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require('body-parser');
-const connectDB = require("../db/db");
+const connectDB = require("./db/db");
 
 const app = express();
 connectDB();
 
-const todoRouter = require('../routes/route');
+const todoRouter = require('./routes/route');
 
 const PORT = process.env.PORT || 5000;
-app.use(cors(
-    {
-        origin: "*",
-        credentials: true,
-    }
-));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
