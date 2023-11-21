e015f1ac03b70705692f72c475186d6a88210770
Koustav <koustavm.aeie2018@nsec.ac.in>
2023-11-21 23:04:30 +0530

    x

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,12 @@ connectDB();
 const todoRouter = require('../routes/route');
 
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors(
+    {
+        origin: "*",
+        credentials: true,
+    }
+));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
