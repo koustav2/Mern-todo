f098a23bdd83cdd69f741f2c03053cb5bb029edb
koustav2 <koustavm.aeie208@nsec.ac.in>
2024-06-23 06:57:17 +0530

    a

--- frontend/src/components/ToDoFilter.jsx
+++ frontend/src/components/ToDoFilter.jsx
@@ -6,7 +6,7 @@ export const TodoFilter = ({
 
     return (
         <section className="container mx-auto mt-8">
-            <div className="flex justify-center gap-4 rounded-md p-4 bg-white p-4 dark:bg-gray-800 transition-all duration-1000">
+            <div className="flex justify-center gap-4 p-4 transition-all duration-1000 bg-white rounded-md dark:bg-gray-800">
                 <button
                     className={`${filter === "all"
                         ? "text-blue-500 hover:text-gray-400"
